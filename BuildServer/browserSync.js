e460b32ac5d91b25a7b68f45f3fdcf4e63e05643
CORP\YuQ <Qiong.Yu@weyerhaeuser.com>
2018-07-20 09:55:43 -0600

    First commit

---
+++ BuildServer/browserSync.js
@@ -0,0 +1,66 @@
+import bs from 'browser-sync';
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+
+// Set "process.env.NODE_ENV" BEFORE calling the Webpack CLI
+process.env.NODE_ENV = "development";
+
+// Some options I'm using in my Webpack config function
+const options = {
+    // should always be true --- BrowserSync without HMR is pretty pointless
+    hot: true, 
+    devServer: true,
+    browserSync: true
+};
+
+// Get the Webpack config with options
+import config from "../webpack.config.dev";
+const bunlder = webpack(config);
+
+// Run Browsersync and use middleware for Hot Module Replacement
+const middleware = [
+    webpackDevMiddleware(bunlder, {
+        // IMPORTANT: dev middleware can't access config, so we should provide publicPath by ourselves
+        publicPath: config.output.publicPath,
+        noInfo: true,
+        // colored output
+        stats: {colors: true}
+        // for other settings:
+        // https://webpack.js.org/guides/development/#webpack-dev-middleware
+    })
+]; 
+
+if (options.hot === true) {
+    // bunlder should be the same as above
+    middleware.push(webpackHotMiddleware(bunlder));
+}
+
+bs({
+    port: 3000,
+    notify: false,
+    server: {
+        baseDir: config.output.publicPath,
+        middleware
+    },
+
+    // No need to watch '*.js' here, webpack will take care of it
+    // including full page reloads if HMR won't work
+    files: [
+        'css/*.css',
+        '*.html'
+    ]
+});
+
+// bs.create().init({
+//     server: "src",
+//     files: ["src/css/*.css"],
+//     plugins: [
+//         {
+//             module: "bs-html-injector",
+//             options: {
+//                 files: ["src/*.html"]
+//             }
+//         }
+//     ]
+// });
---
+++ webpack.config.dev.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    devtool: 'inline-source-map',
+    entry: [
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["latest"]
+                    }
+                }]
+            },
+            {
+                test: /.\css$/,
+                include: __dirname + "./src/css",
+                exclude: __dirname + "./src/js",
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+        // ,
+        // loaders: [
+        //     {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+        //     {test: /\.css$/, loaders: ['style', 'css']}
+        // ]
+    },
+    plugins: [
+        // new UglifyJsPlugin()
+    ]
+};
