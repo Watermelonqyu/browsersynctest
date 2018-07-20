179f4f5ba277962d93216a262ebbb0d90db0d357
CORP\YuQ <Qiong.Yu@weyerhaeuser.com>
2018-07-20 17:15:13 -0600

    Finished eslint module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./index.css";
 import numeral from 'numeral';
 
-debugger;
+/* eslint-disable no-console */
+// debugger;
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(courseValue);
 console.log('I would pay ${courseValue} for this awesome course!');
