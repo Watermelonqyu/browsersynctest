01d28a312ef727dc9616e8149ed408948acc7bc0
CORP\YuQ <Qiong.Yu@weyerhaeuser.com>
2018-07-21 15:00:15 -0600

    Integrated with unit test with jsdom

---
+++ src/index.test.js
@@ -0,0 +1,36 @@
+import {expect} from 'chai';
+import fs from 'fs';
+
+var jsdom;
+try {
+    jsdom = require("jsdom/lib/old-api.js"); // jsdom >= 10.x
+} catch (e) {
+    jsdom = require("jsdom"); // jsdom <= 9.x
+}
+describe('Our first test', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', () => {
+    it('should say My First Heading', (done) => {
+        const index = fs.readFileSync('./src/index.html', 'utf-8');
+        jsdom.env(index, function(err, window) {
+            const h1 = window.document.getElementById('h1');
+            expect(h1.innerHTML).to.equal('My First Heading');
+            done();
+            window.close();
+        });
+        // const options = {
+        //     resources: 'usable',
+        //     runScripts: 'dangerously'
+        // };
+
+        // jsdom.JSDOM.fromFile('./src/index.html', options).then(dom => {
+        //     const h1 = dom.window.document.getElementById('h1')[0];
+        //     expect(h1.innerHTML).to.equal('My First Heading?');
+        //     done();
+        // });
+    });
+});
